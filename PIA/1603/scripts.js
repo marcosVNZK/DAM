7ad69c2d7de474c825af4463a71d68a4402ea577
marcosVNZK <160616723+marcosVNZK@users.noreply.github.com>
2024-03-19 18:21:16 -0300

    Add files via upload

---
+++ PIA/1603/scripts.js
@@ -0,0 +1,8 @@
+function recebe (){
+
+    var x = new Array
+    x.floor(Math.random(x) * 10 + 1)
+const v = document.getElementById("V32") = x;
+}
+return x;
+let x1 = 
---
+++ PIA/Atividade  Pia/restaurante.js
@@ -0,0 +1,10 @@
+function clica() {
+console.log("Alerta");
+alert("O link foi clicado.")
+}
+
+function clique(img) {
+    return .style.width ="100";
+  }
+
+
